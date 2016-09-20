fb42c210f1971572a0c656da8a2bb40d2497053e
pachecojm23 <pachecojm23@gmail.com>
2016-09-20 06:49:28 -0600

    Agregado Gestión Aula

---
+++ resources/views/gestion.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Bill Gates Shool & American Eagles Academy</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Kids School Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- js -->
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+<script src="js/jquery-1.11.1.min.js"></script>
+<!-- //js -->
+<link href='//fonts.googleapis.com/css?family=Cabin:400,400italic,500,500italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Peralta' rel='stylesheet' type='text/css'>
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+</head>
+	
+<body>
+<!-- body -->
+	<div class="body-content">
+		<div class="container">
+			<div class="body-content1">
+			<!-- header -->
+				<div class="logo-search">
+					<div class="logo">
+						<h1><a href="index.html">Bill<span class="color"> </span><span class="color1"></span><span class="color2">Gates</span> <span>School</span> <i>An Integral Education for a future of Success</i></a></h1>
+					</div>
+					<!--div class="search">
+						<form>
+							<input type="text" value="Search Here..." onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search Here...';}" required="">
+							<input type="submit" value=" " >
+						</form>
+					</div-->
+					<div class="clearfix"> </div>
+				</div>
+			<!-- //header -->
+			
+			@include('home/navbar')
+						
+			@include ('gestiontabs')
+			</div>
+		</div>
+	</div>
+<!-- //body -->
+
+@include('home/footer')
+
+<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+									$(document).ready(function() {
+										/*
+										var defaults = {
+								  			containerID: 'toTop', // fading element id
+											containerHoverID: 'toTopHover', // fading element hover id
+											scrollSpeed: 1200,
+											easingType: 'linear' 
+								 		};
+										*/
+										
+										$().UItoTop({ easingType: 'easeOutQuart' });
+										
+									});
+	</script>
+<!-- //here ends scrolling icon -->
+<!-- for bootstrap working -->
+	<script src="js/bootstrap.js"></script>
+<!-- //for bootstrap working -->
+</body>
+</html>
---
+++ resources/views/gestiontabs.blade.php
@@ -0,0 +1,189 @@
+<div class="grid_3 grid_5">
+    <center><h3 class="bars">GESTIÓN AULA</h3></center>
+	<div class="bs-example bs-example-tabs" role="tabpanel" data-example-id="togglable-tabs">
+	    <ul id="myTab" class="nav nav-tabs" role="tablist">
+		    <li role="presentation" class="active"><a href="#home" id="home-tab" role="tab" data-toggle="tab" aria-controls="home" aria-expanded="true">Guía de Exámenes</a></li>
+			<li role="presentation"><a href="#familia" role="tab" id="familia-tab" data-toggle="tab" aria-controls="familia">Proyecto Leo en Familia</a></li>
+            <li role="presentation"><a href="#horarios" role="tab" id="horarios-tab" data-toggle="tab" aria-controls="profile">Horario Escolar</a></li>
+			<!--li role="presentation" class="dropdown">
+			    <a href="#" id="myTabDrop1" class="dropdown-toggle" data-toggle="dropdown" aria-controls="myTabDrop1-contents">Dropdown <span class="caret"></span></a>
+				<ul class="dropdown-menu" role="menu" aria-labelledby="myTabDrop1" id="myTabDrop1-contents">
+				    <li><a href="#dropdown1" tabindex="-1" role="tab" id="dropdown1-tab" data-toggle="tab" aria-controls="dropdown1">@fat</a></li>
+					<li><a href="#dropdown2" tabindex="-1" role="tab" id="dropdown2-tab" data-toggle="tab" aria-controls="dropdown2">@mdo</a></li>
+				</ul>
+			</li-->
+		</ul>
+		
+        <div id="myTabContent" class="tab-content">
+		    <div role="tabpanel" class="tab-pane fade in active" id="home" aria-labelledby="home-tab">
+			    <table class="table">
+				    <thead>
+					    <tr>
+						    <th>Sección</th>
+							<th>Link Guía</th>
+						</tr>
+					</thead>
+					<tbody>
+					    <tr>
+						    <td>Toddler</td>
+							<td><a href="http://examstoddler.blogspot.com/" target="_blank">Link</a></td>
+						</tr>
+						<tr>
+							<td>Kinder 4</td>
+							<td><a href="http://examskinder4.blogspot.com/" target="_blank">Link</a></td>
+						</tr>
+						<tr>
+							<td>Kinder 5</td>
+							<td><a href="http://examskinder5.blogspot.com/" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+							<td>Kinder 6</td>
+							<td><a href="http://examskinder6.blogspot.com/" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+							<td>First Grade</td>
+							<td><a href="http://examsfirstgrade.blogspot.com/ " target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+						    <td>Second Grade</td>
+							<td><a href="http://examssecondgrade.blogspot.com/ " target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+						    <td>Third Grade</td>
+							<td><a href="http://examsthirdgrade.blogspot.com/ " target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+						    <td>Fourth Grade</td>
+							<td><a href="http://examsfourthgrade.blogspot.com/  " target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+							<td>Fifth Grade</td>
+							<td><a href="http://examsfifthgrade.blogspot.com/" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+						    <td>Sixth Grade</td>
+							<td><a href="http://examssixthgrade.blogspot.com/" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+						    <td>Seventh Grade</td>
+							<td><a href="http://examsseventhgrade.blogspot.com/" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+							<td>Eight Grade</td>
+							<td><a href="http://examseightgrade.blogspot.com/" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+						    <td>Nineth Grade</td>
+						    <td><a href="http://examsninethgrade.blogspot.com/" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+						    <td>Tenth Grade</td>
+							<td><a href="http://examstenthgrade.blogspot.com/" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+						    <td>Eleventh Grade</td>
+							<td><a href="http://examseleventhgrade.blogspot.com/" target="_blank">Link</a></td>
+						</tr>
+					</tbody>
+				</table>           
+			</div>
+		<div role="tabpanel" class="tab-pane fade" id="familia" aria-labelledby="familia-tab">
+		    <table class="table">
+					<tbody>
+					    <tr>
+						    <td>Kindergarten</td>
+							<td><a href="http://leoenfamiliaparvularia.blogspot.com/" target="_blank">Link</a></td>
+						</tr>
+						<tr>
+							<td>Elementary</td>
+							<td><a href="http://leoenfamiliaelementary.blogspot.com/" target="_blank">Link</a></td>
+						</tr>
+						<tr>
+							<td>Junior High School</td>
+							<td><a href="http://leoenfamiliaparajuniorhigh.blogspot.com/" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+							<td>High School</td>
+							<td><a href="http://leoenfamiliahighschool.blogspot.com/" target="_blank">Link</a></td>
+						</tr> 
+					</tbody>
+				</table>
+		</div>
+		<div role="tabpanel" class="tab-pane fade" id="horarios" aria-labelledby="horarios-tab">
+		    <table class="table">
+				    <thead>
+					    <tr>
+						    <th>Sección</th>
+							<th>Horario 2016</th>
+						</tr>
+					</thead>
+					<tbody>
+					    <tr>
+						    <td>Toddler</td>
+							<td><a href="images/Schedule_Toddler.jpg" target="_blank">Link</a></td>
+						</tr>
+						<tr>
+							<td>Kinder 4</td>
+							<td><a href="images/Schedule_K4.jpg" target="_blank">Link</a></td>
+						</tr>
+						<tr>
+							<td>Kinder 5</td>
+							<td><a href="images/Schedule_K5.jpg" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+							<td>Kinder 6</td>
+							<td><a href="images/Schedule_PreSchool.jpg" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+							<td>First Grade</td>
+							<td><a href="images/Schedule_1Grade.jpg" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+						    <td>Second Grade</td>
+							<td><a href="images/Schedule_2Grade.jpg" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+						    <td>Third Grade</td>
+							<td><a href="images/Schedule_3Grade.jpg" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+						    <td>Fourth Grade</td>
+							<td><a href="images/Schedule_4Grade.jpg" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+							<td>Fifth Grade</td>
+							<td><a href="images/Schedule_5Grade.jpg" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+						    <td>Sixth Grade</td>
+							<td><a href="images/Schedule_6Grade.jpg" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+						    <td>Seventh Grade</td>
+							<td><a href="images/Schedule_7Grade.jpg" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+							<td>Eight Grade</td>
+							<td><a href="images/Schedule_8Grade.jpg" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+						    <td>Nineth Grade</td>
+						    <td><a href="http://examsninethgrade.blogspot.com/" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+						    <td>Tenth Grade</td>
+							<td><a href="#" target="_blank">Link</a></td>
+						</tr>
+                        <tr>
+						    <td>Eleventh Grade</td>
+							<td><a href="#" target="_blank">Link</a></td>
+						</tr>
+					</tbody>
+				</table>   
+		</div>
+		<div role="tabpanel" class="tab-pane fade" id="dropdown2" aria-labelledby="dropdown2-tab">
+		    <p>Trust fund seitan letterpress, keytar raw denim keffiyeh etsy art party before they sold out master cleanse gluten-free squid scenester freegan cosby sweater. Fanny pack portland seitan DIY, art party locavore wolf cliche high life echo park Austin. Cred vinyl keffiyeh DIY salvia PBR, banh mi before they sold out farm-to-table VHS viral locavore cosby sweater. Lomo wolf viral, mustache readymade thundercats keffiyeh craft beer marfa ethical. Wolf salvia freegan, sartorial keffiyeh echo park vegan.
+            </p>
+		</div>
+	</div>
+</div>
